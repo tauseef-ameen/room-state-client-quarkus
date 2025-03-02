e56bc0a666da410556e6c4f823f43867fbce672a
tauseef-ameen <tauseef.125002@gmail.com>
2025-03-02 12:39:47 +0100

    Adapted roomId in client

--- src/main/java/org/acme/state/RoomStateClient.java
+++ src/main/java/org/acme/state/RoomStateClient.java
@@ -9,9 +9,9 @@ public class RoomStateClient {
 
     public List<RoomState> allRoomStates() {
         return List.of(new RoomState(1, "Available"),
-                new RoomState(2, "Maintenance"),
-                new RoomState(3, "Available"),
-                new RoomState(4, "Occupied"),
-                new RoomState(5, "Available"));
+                new RoomState(11, "Maintenance"),
+                new RoomState(22, "Available"),
+                new RoomState(33, "Occupied"),
+                new RoomState(44, "Available"));
     }
 }
